c79e7e2ec74d2f4ecd2bb32d99390cb03dccafad
chereshnyabtw <chereshnyabtw@icloud.com>
2020-12-20 13:32:44 +0500

    Initial commit

---
+++ backend/src/main/java/com/chereshnevvv/syndicate/SyndicateApplication.java
@@ -0,0 +1,13 @@
+package com.chereshnevvv.syndicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyndicateApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/chereshnevvv/syndicate/SyndicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chereshnevvv.syndicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyndicateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
